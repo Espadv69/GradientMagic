bc13d2c6a2eadacbc1c6bcb812d531a31ad30b76
Espadv69 <aitorjob69@gmail.com>
2025-03-25 16:41:17 +0100

    Add imports and initial setup for GradientGenerator component

--- src/components/GradientGenerator.jsx
+++ src/components/GradientGenerator.jsx
@@ -1,3 +1,8 @@
+import { useState } from 'react'
+import { SketchPicker } from 'react-color'
+
+import './GradientGenerator.css'
+
 const GradientGenerator = () => {}
 
 export default GradientGenerator
