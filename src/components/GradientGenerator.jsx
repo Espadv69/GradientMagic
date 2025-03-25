149e3eb4d0efadecdec633d90f79ed44efca8863
Espadv69 <aitorjob69@gmail.com>
2025-03-25 16:42:10 +0100

    Add state management for color and angle in GradientGenerator component

--- src/components/GradientGenerator.jsx
+++ src/components/GradientGenerator.jsx
@@ -3,6 +3,10 @@ import { SketchPicker } from 'react-color'
 
 import './GradientGenerator.css'
 
-const GradientGenerator = () => {}
+const GradientGenerator = () => {
+  const [firstColor, setFirstColor] = useState('#ff0000')
+  const [secondColor, setSecondColor] = useState('#0000ff')
+  const [angle, setAngle] = useState(90)
+}
 
 export default GradientGenerator
