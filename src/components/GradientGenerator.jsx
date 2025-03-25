4c71fcd1ca824f161ef184ffd2217663b804bf43
Espadv69 <aitorjob69@gmail.com>
2025-03-25 16:43:52 +0100

    Add gradient style to GradientGenerator component

--- src/components/GradientGenerator.jsx
+++ src/components/GradientGenerator.jsx
@@ -7,6 +7,13 @@ const GradientGenerator = () => {
   const [firstColor, setFirstColor] = useState('#ff0000')
   const [secondColor, setSecondColor] = useState('#0000ff')
   const [angle, setAngle] = useState(90)
+
+  const gradientStyle = {
+    background: `linear-gradient(${angle}deg, ${firstColor}, ${secondColor})`,
+    width: '100%',
+    height: '200px',
+    marginTop: '.8rem',
+  }
 }
 
 export default GradientGenerator
